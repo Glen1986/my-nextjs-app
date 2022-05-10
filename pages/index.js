8b7747a42ae2e3b79143517a739aac93a042a764
Glen1986 <escobar.glen@gmail.com>
2022-05-10 17:36:55 -0300

    next-js-app-dinamic-routes-img

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,16 @@
+import Image from 'next/image'
 import Link from 'next/link'
+import Comet from '/public/img/comet.png'
 export default function Home() {
+    // <Image src="/img/comet.png" width={400} height={400} />
+
     return (
         <div>
             <p>Index</p>
             <Link href="/chancho">chancho</Link>
             <br />
             <Link href="/Componente">componente</Link>
+            <Image src={Comet} width={400} height={400}></Image>
         </div>
     )
 }
